f7190d7ab2984795fab97a0d5a90bea593ec3dbd
nithinmohan114 <nithinmohan114@gmail.com>
2018-06-26 16:35:01 +0530

    Add models and views

--- my_app/test_sample.py
+++ my_app/test_sample.py
@@ -1,5 +1,7 @@
+from my_app.models import Line
 
 
 def test_sample():
     assert 1 == 1
     assert 2 == 2
+    assert Line.text is None
--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def homePageView(request):
+        return HttpResponse('Hello, World!')
