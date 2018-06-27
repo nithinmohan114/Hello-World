2c3d96ef0ca1c23c193e7efb83b4c73c9aa024f0
nithinmohan114 <nithinmohan114@gmail.com>
2018-06-27 11:40:16 +0530

    remove app registry issue

--- my_app/test_sample.py
+++ my_app/test_sample.py
@@ -1,7 +1,11 @@
+import django
+django.setup()
 from my_app.models import Line
 
 
 def test_sample():
     assert 1 == 1
     assert 2 == 2
+    # a = Line.objects.create(text="hello")
+    # assert a.text == "hello"
     assert Line.text is None
